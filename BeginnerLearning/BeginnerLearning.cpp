3ffabe6f32b4c121f00fd84dfc1b7888c0b96da4
PetarPehchevski3D <petarpehchevski3d@gmail.com>
2023-09-17 17:45:58 +0200

    commit

--- BeginnerLearning/BeginnerLearning.cpp
+++ BeginnerLearning/BeginnerLearning.cpp
@@ -6,8 +6,10 @@
 int main()
 {
     std::cout << "Hello World!\n";
-    std::cout << "First C++ line\n";
-    std::cout << "geyyy";
+    std::cout << "print\n";
+    std::cout << "this is new branch";
+    std::cout << "this is neeeeeeeeeeeeeew branch";
+
 
 
 
