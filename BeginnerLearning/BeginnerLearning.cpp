befaef7adced0a53ec3e666986e235c8e9442d22
PetarPehchevski3D <petarpehchevski3d@gmail.com>
2023-09-17 16:18:20 +0200

    added few more lines of print

--- BeginnerLearning/BeginnerLearning.cpp
+++ BeginnerLearning/BeginnerLearning.cpp
@@ -6,6 +6,8 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << "First C++ line\n";
+    std::cout << "testing out the commit thing\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
